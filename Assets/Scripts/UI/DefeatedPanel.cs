b10973afa7b908b796cb2cfe1020a659ac5286ed
mustafamese <mustafamese099@gmail.com>
2020-08-27 14:04:15 +0300

    -Defeated canvas re-built.

--- Assets/Scripts/UI/DefeatedPanel.cs
+++ Assets/Scripts/UI/DefeatedPanel.cs
@@ -1,26 +1,77 @@
-﻿using DungeonRush.Managers;
+﻿using DungeonRush.Cards;
+using DungeonRush.Managers;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DefeatedPanel : MonoBehaviour
 {
+    private const string DUNGEON = ". dungeon";
+    private const string ENCOUNTER = ". encounter";
+    private const string GOLD = "Gold: ";
+    private const string XP = "Experience: ";
+
     [SerializeField] GameObject defeatedPanel;
     [SerializeField] GameManager gameManager;
 
+    [SerializeField] TextMeshProUGUI dungeonText;
+    [SerializeField] TextMeshProUGUI encounterText;
+    [SerializeField] TextMeshProUGUI goldText;
+    [SerializeField] TextMeshProUGUI xpText;
+
+    private PlayerCard player;
+
+    [SerializeField] List<GameObject> panels = new List<GameObject>();
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerCard>();
+    }
 
     public void SetDefeat()
     {
         GameManager.gameState = GameState.STOP_GAME;
         defeatedPanel.SetActive(true);
-
-        StartCoroutine(GotoMainMenu());
+        DeactivatePanels();
+        SetValues();
     }
 
-    private IEnumerator GotoMainMenu()
+    public void MainMenu()
     {
-        yield return new WaitForSeconds(1f);
+        StartCoroutine(Fading());
+    }
+
+    private IEnumerator Fading()
+    {
         yield return gameManager.FadeOut();
         GameManager.gameState = GameState.END;
+    }
+
+    private void SetValues()
+    {
+        string s = "";
+        int index = SceneManager.GetActiveScene().buildIndex + 1;
+
+        s = index.ToString() + ENCOUNTER;
+        encounterText.text = s;
+
+        s = 1.ToString() + DUNGEON;
+        dungeonText.text = s;
+
+        s = GOLD + player.Coins.ToString();
+        goldText.text = s;
+
+        s = XP + player.Experience.ToString();
+        xpText.text = s;
+    }
+
+    private void DeactivatePanels()
+    {
+        for (int i = 0; i < panels.Count; i++)
+        {
+            panels[i].SetActive(false);
+        }
     }
 }
